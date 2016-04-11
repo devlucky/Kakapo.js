77986eae4a22a4f5f3a2a55fcddcdfbb8eff00fe
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-11 03:41:53 +0200

    Change karma launcher to firefox, since travis doesnt have chrome

--- .karma.conf.js
+++ .karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function(config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     frameworks: ['browserify', 'tap'],
     files: [
       'tests/karma.js'
