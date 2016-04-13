607b78e3e42e8726e1e46267576bf152da3dc6ef
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-13 18:43:44 +0200

    wrapper for fake fetch & XHR

---
+++ src/interceptors/interceptor.js
@@ -0,0 +1,34 @@
+import queryString from 'query-string';
+import pathMatch from 'path-match';
+import parseUrl from 'parse-url';
+
+export const interceptor = (serverRoutes, fakeService) => {
+  const getRoute = (handlers, pathname) => {
+    const matchesPathname = path => pathMatch()(path)(pathname);
+    const route = Object.keys(handlers).find(matchesPathname);
+
+    return route || undefined;
+  };
+
+  const extractUrl = (url, method) => ({
+    handlers: serverRoutes[method],
+    pathname: parseUrl(url).pathname
+  });
+
+  const getHandler = (url, method) => {
+    const { handlers, pathname } = extractUrl(url, method);
+    const route = getRoute(handlers, pathname);
+
+    return route ? handlers[route] : undefined;
+  };
+
+  const getParams = (url, method) => {
+    const {handlers, pathname} = extractUrl(url, method);
+    const matchesPathname = path => pathMatch()(path)(pathname);
+    const route = getRoute(handlers, pathname);
+
+    return route ? matchesPathname(route) : undefined;
+  };
+
+  return fakeService.bind(null, {getHandler, getParams});
+};
