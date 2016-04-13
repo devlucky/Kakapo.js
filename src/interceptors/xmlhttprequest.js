361a970548ae140d8fe1ff8e826eb8a2af929736
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2016-04-13 17:58:34 +0200

    Refactor XHR interceptor to match fetch more

--- src/interceptors/xmlhttprequest.js
+++ src/interceptors/xmlhttprequest.js
@@ -1,58 +1,48 @@
-import pathToRegexp from 'path-to-regexp';
-let routes;
-const nativeXMLHttpRequest = window.XMLHttpRequest;
+import queryString from 'query-string';
+import pathMatch from 'path-match';
+import parseUrl from 'parse-url';
 
-export const fakeXMLHttpRequest = (serverRoutes) => {
-  routes = serverRoutes;
+const NativeXMLHttpRequest = window.XMLHttpRequest;
 
-  return class fakeXMLHttpRequest {
-    constructor() {
-      this.xhr = new nativeXMLHttpRequest(); 
-    }
-
-    open(method, url) {
-      this.method = method;
-      this.url = url;
-      this.xhr.open(method, url);
-    }
-
-    send() {
-      let self = this;
-      const url = self.url;
-      const onreadystatechange = self.onreadystatechange;
-      const method = self.method;
-      const methodRoutes = routes[method];
-      let routeHandler;
-      let params;
-
-      Object.keys(methodRoutes).forEach((path) => {
-        let routeRegex = pathToRegexp(path);
-
-        if (!routeHandler && routeRegex.exec(url)) {
-          routeHandler = methodRoutes[path];
-          params = routeRegex.exec(url);
-        }
-      });
-
-      if (routeHandler && onreadystatechange) {
-        self.readyState = 4;
-        self.status = 200; //TODO: Support custom status codes
-        self.responseText = routeHandler({params});
-        onreadystatechange();
-        return;
-      }
-      
-      this.xhr.onreadystatechange = function() {
-        self.readyState = this.readyState;
-        self.status = this.status;
-        self.responseText = this.responseText;
-        onreadystatechange.call(this);
-      };
-      this.xhr.send();
-    }
+export const fakeXMLHttpRequest = serverRoutes => class fakeXMLHttpRequest {
+  constructor() {
+    this.xhr = new NativeXMLHttpRequest();
   }
-}
+
+  open(method, url) {
+    this.method = method;
+    this.url = url;
+    this.xhr.open(method, url);
+  }
+
+  send() {
+    const handlers = serverRoutes[this.method];
+    const pathname = parseUrl(this.url).pathname;
+    const matchesPathname = path => pathMatch()(path)(pathname);
+    const route = Object.keys(handlers).find(matchesPathname);
+
+    if (route && this.onreadystatechange) {
+      const handler = handlers[route];
+      const params = matchesPathname(route);
+
+      this.readyState = 4;
+      this.status = 200; //TODO: Support custom status codes
+      this.responseText = handler({params});
+      this.onreadystatechange();
+      return;
+    }
+
+    this.xhr.onreadystatechange = () => {
+      this.readyState = this.xhr.readyState;
+      this.status = this.xhr.status;
+      this.responseText = this.xhr.responseText;
+      this.onreadystatechange.call(this.xhr);
+    };
+
+    this.xhr.send();
+  }
+};
 
 export const reset = () => {
-  window.XMLHttpRequest = nativeXMLHttpRequest;
-}
+  window.XMLHttpRequest = NativeXMLHttpRequest;
+};
