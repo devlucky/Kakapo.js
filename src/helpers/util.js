13e9e6f1b9ccecc00f8d1840f420dbdb8f8bb55a
Oskar Cieslik <oskar.cieslik@gmail.com>
2016-05-10 23:18:55 +0200

    style(helpers/util): fixes according to airbnb-javascript guide

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,11 +1,5 @@
-export const randomIndex = (arr) => {
-  return Math.floor(Math.random() * arr.length);
-}
+export const randomIndex = arr => Math.floor(Math.random() * arr.length);
 
-export const randomItem = (arr) => {
-  return arr[randomIndex(arr)];
-}
+export const randomItem = arr => arr[randomIndex(arr)];
 
-export const lastItem = (arr) => {
-  return arr[arr.length - 1];
-}
+export const lastItem = arr => arr[arr.length - 1];
