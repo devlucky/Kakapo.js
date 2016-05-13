f3de1134c6c7f18acec9c65bb115480dfdc3311d
Oskar Cieslik <oskar.cieslik@gmail.com>
2016-05-13 05:59:19 +0200

    docs(database/recordFactory): Add documentation for recordFactory

--- src/Database/recordFactory.js
+++ src/Database/recordFactory.js
@@ -1,12 +1,38 @@
 import _ from 'lodash';
 
+/**
+ * Returns record updated according to changes on it's instance.
+ *
+ * @param {Object} record - instance of record with changes
+ * @param {string} collectionName - name of record's collection
+ * @param {Object} recordStore - database's store with records
+ *
+ * @returns {Object}
+ * @private
+ */
 const updateRecord = (record, collectionName, recordStore) => {
   const originalRecord = _.find(recordStore.get(collectionName), { id: record.id });
   return _.assign(originalRecord, record);
 };
 
+/**
+ * Returns record's copy with multiple useful methods assigned to it.
+ *
+ * @param {Object} record - record to extend with new methods
+ * @param {string} collectionName - name of record's collection
+ * @param {Object} recordStore - database's store with records
+ *
+ * @returns {Object}
+ * @private
+ */
 export const recordFactory = (record, collectionName, recordStore) => {
   const recordExtension = {
+    /**
+     * Returns record updated via *updateRecord*.
+     *
+     * @returns {Object}
+     * @private
+     */
     save() {
       return updateRecord(this, collectionName, recordStore);
     },
