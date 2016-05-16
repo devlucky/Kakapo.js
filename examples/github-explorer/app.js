9e4568a6bd7d1ae44a5d3e323489a9ff00c6fd97
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-05-16 21:29:03 +0200

    Github explorer wip

---
+++ examples/github-explorer/app.js
@@ -0,0 +1,26 @@
+(function() {
+  const host = 'https://api.github.com';
+
+  const loadUsers = () => {
+    const xhr = $.get(`users`);
+    // window.XMLHttpRequest = null
+    // $.get(`${host}/users`).then(r => {
+    xhr.then(r => {
+      debugger;
+    });
+
+    xhr.error(err => {
+      console.log(err);
+    });
+
+    // xhr.fail(err => {
+    //   debugger
+    // })
+  };
+
+  const init = () => {
+    loadUsers();
+  };
+
+  document.addEventListener("DOMContentLoaded", init);
+})();
---
+++ examples/github-explorer/kakapo-config.js
@@ -0,0 +1,9 @@
+(function() {
+  // const router = new Kakapo.Router({host: 'https://api.github.com'});
+  // const router = new Kakapo.Router();
+  const db = new Kakapo.Database();
+
+  // router.get('/users', () => {
+  //   debugger
+  // });
+})();
