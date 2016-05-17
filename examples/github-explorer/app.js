0717ac21dafadbbfd6966d254e5f716179cf16bd
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-05-17 23:41:34 +0200

    Fake user list and detail

--- examples/github-explorer/app.js
+++ examples/github-explorer/app.js
@@ -1,25 +1,60 @@
 (function() {
   const host = 'https://api.github.com';
+  let users = [];
+
+  const render = () => {
+    const usersHtml = users.map(userTemplate).join('');
+
+    $('#user-list').html(usersHtml);
+  };
+
+  const userTemplate = (user) => {
+    const login = user.login;
+
+    return `
+    <li class="collection-item avatar" data-login="${login}">
+      <img src="${user.avatar_url}" alt="" class="circle">
+      <span class="title">${login}</span>
+      <p>
+        ${user.html_url}
+      </p>
+      <a href="#!" class="secondary-content">
+        <i class="material-icons">send</i>
+      </a>
+    </li>
+    `;
+  };
 
   const loadUsers = () => {
     const xhr = $.get(`${host}/users`);
-    // window.XMLHttpRequest = null
-    // $.get(`${host}/users`).then(r => {
-    xhr.then(r => {
+
+    return xhr.then(r => {
+      users = r;    
+    });
+  };
+
+  const loadUser = (login) => {
+    const xhr = $.get(`${host}/users/${login}`);
+
+    return xhr.then(r => {
       debugger;
     });
+  }
 
-    xhr.error(err => {
-      console.log(err);
-    });
+  const addEvents = () => {
+    $('#user-list').on('click', 'li', onUserClick);
+  };  
 
-    // xhr.fail(err => {
-    //   debugger
-    // })
+  const onUserClick = function() {
+    const login = $(this).attr('data-login');
+
+    loadUser(login);
   };
 
   const init = () => {
-    loadUsers();
+    loadUsers().then(render);
+
+    addEvents();
   };
 
   document.addEventListener("DOMContentLoaded", init);
--- examples/github-explorer/kakapo-config.js
+++ examples/github-explorer/kakapo-config.js
@@ -1,9 +1,57 @@
 (function() {
-  // const router = new Kakapo.Router({host: 'https://api.github.com'});
-  // const router = new Kakapo.Router();
+  const host = 'https://api.github.com';
+  const router = new Kakapo.Router({host});
   const db = new Kakapo.Database();
+  const userSerializer = (user) => {
+    return Object.assign({}, user, {
+      name: "Tom Preston-Werner",
+      company: null,
+      blog: "http://tom.preston-werner.com",
+      location: "San Francisco",
+      email: "tom@mojombo.com",
+      hireable: null,
+      bio: null,
+      public_repos: 60,
+      public_gists: 65,
+      followers: 19250,
+      following: 11,
+      created_at: "2007-10-20T05:24:19Z",
+      updated_at: "2016-05-10T19:31:30Z"
+    });
+  };
 
-  // router.get('/users', () => {
-  //   debugger
-  // });
+  db.register('user', (faker) => {
+    const login = faker.internet.userName();
+
+    return {
+      login: login,
+      avatar_url: faker.internet.avatar,
+      gravatar_id: "",
+      url: `https://api.github.com/users/${login}`,
+      html_url: `https://github.com/${login}`,
+      followers_url: `https://api.github.com/users/${login}/followers`,
+      following_url: `https://api.github.com/users/${login}/following{/other_user}`,
+      gists_url: `https://api.github.com/users/${login}/gists{/gist_id}`,
+      starred_url: `https://api.github.com/users/${login}/starred{/owner}{/repo}`,
+      subscriptions_url: `https://api.github.com/users/${login}/subscriptions`,
+      organizations_url: `https://api.github.com/users/${login}/orgs`,
+      repos_url: `https://api.github.com/users/${login}/repos`,
+      events_url: `https://api.github.com/users/${login}/events{/privacy}`,
+      received_events_url: `https://api.github.com/users/${login}/received_events`,
+      type: "User",
+      site_admin: true
+    }
+  });
+
+  db.create('user', 30);
+
+  router.get('/users', () => {
+    return db.all('user');
+  });
+
+  router.get('/users/:login', (request) => {
+    const login = request.params.login;
+    
+    return userSerializer(db.findOne('user', {login}));
+  });
 })();
