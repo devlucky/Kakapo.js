b36414757b27100236463e3f9b23f37b650e7702
Oskar Cieslik <oskar.cieslik@gmail.com>
2016-05-21 20:00:23 +0200

    Finish tests for xhrInterceptor.open

--- test/specs/xhrInterceptor.spec.js
+++ test/specs/xhrInterceptor.spec.js
@@ -7,19 +7,32 @@ import { nativeXHR } from '../../src/helpers/nativeServices';
 export const xhrInterceptorSpec = () => {
   test('xhrInterceptor . constructor', (assert) => {
     const XMLHttpRequestInterceptor = fakeService({});
-    const xhrInterceptor = new XMLHttpRequestInterceptor();
+    const xhr = new XMLHttpRequestInterceptor();
 
     const xhrInterceptorOwnProps = ['xhr', 'getHandler', 'getParams'];
 
     assert.ok(
-      xhrInterceptorOwnProps.every(_.partial(_.has, xhrInterceptor)),
-      'Defines own properties to xhrInterceptor.'
+      xhrInterceptorOwnProps.every(_.partial(_.has, xhr)),
+      'defines own properties to xhrInterceptor.'
     );
 
     assert.ok(
-      _.keysIn(new nativeXHR()).every(_.partial(_.has, xhrInterceptor)),
-      'Inherits properties from native XHR object.'
+      _.keysIn(new nativeXHR()).every(_.partial(_.has, xhr)),
+      'inherits properties from native XHR object.'
     );
+
+    assert.equal(xhr.readyState, 0, 'sets readyState to 0 as default.');
+
+    assert.end();
+  });
+
+  test('xhrInterceptor . open', (assert) => {
+    const XMLHttpRequestInterceptor = fakeService({});
+    const xhr = new XMLHttpRequestInterceptor();
+
+    xhr.open('GET', '/does_not_exist', true);
+
+    assert.equal(xhr.readyState, 1, 'sets readyState to 1 after being called.');
 
     assert.end();
   });
