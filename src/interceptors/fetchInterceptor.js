0d1bc49ab06e4aae8c920a673163eb27d544f774
Palash Karia <palash@locus.sh>
2018-01-19 19:42:37 +0530

    using Object.assign

--- src/interceptors/fetchInterceptor.js
+++ src/interceptors/fetchInterceptor.js
@@ -39,7 +39,7 @@ const fakeService = helpers => (url, options = {}) => {
     ));
   }
 
-  const result = fakeResponse(response.body, { headers: response.headers, status: response.code, ...response.options });
+  const result = fakeResponse(response.body, Object.assign({}, response.options, { headers: response.headers, status: response.code } );
   return new Promise((resolve, reject) => setTimeout(
     () => {
       if (response.error) { return reject(result); }
