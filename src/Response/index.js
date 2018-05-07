a1ffe94de7a4d81c692e5edaf9f6d65c14e99193
Jimmy Luong <jluong@atlassian.com>
2018-05-07 14:32:00 +1000

    feat: add wrap helper method to response class

--- src/Response/index.js
+++ src/Response/index.js
@@ -4,7 +4,7 @@ export class Response {
   code: number;
   body: any;
   headers: { [header: string]: string };
-  
+
   constructor(
     code: number = 200,
     body: any = {},
@@ -21,5 +21,15 @@ export class Response {
 
   get ok() {
     return this.code >= 200 && this.code <= 299;
+  }
+
+  static wrap(response: any): Response {
+    if (response instanceof Response) {
+      return response;
+    } else {
+      return new Response(200, response, {
+        "content-type": "application/json; charset=utf-8"
+      });
+    }
   }
 }
