de4cd18311af603dc88bbb87fb0fbe6cb86f628c
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-05-09 16:49:23 +1000

    wip

--- examples/react/example/app.tsx
+++ examples/react/example/app.tsx
@@ -18,6 +18,16 @@ router.get('/promise/files', () => {
   });
 });
 
+router.post('/promise/file', async () => {
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  return new Promise(resolve => {
+    resolve({
+      status: 'success'
+    });
+  });
+});
+
 server.use(router);
 
 export interface AppState {
@@ -32,6 +42,7 @@ export default class App extends Component <{}, AppState> {
   componentDidMount() {
     this.getFilesXHR();
     this.getFilesPromiseXHR();
+    this.postFilePromiseXHR();
   }
 
   async getFilesFetch() {
@@ -56,6 +67,16 @@ export default class App extends Component <{}, AppState> {
     xhr.open('GET', '/promise/files');
     xhr.onload = () => {
       console.log('promise response', xhr.response);
+    };
+    xhr.send();
+  }
+
+  postFilePromiseXHR() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/promise/file');
+    xhr.onload = () => {
+      console.log('promise post response', xhr.response);
     };
     xhr.send();
   }
