8733f84cf38cbf25481eead6d8b9e367acbbf822
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2019-12-06 10:58:11 +1100

    Remove generated JS files

--- examples/react/example/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var app_1 = require("./app");
-ReactDOM.render(<app_1.default />, document.getElementById('app'));
